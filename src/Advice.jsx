13343f6912514dba22df6f6ab691e003b6e749cc
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-10-02 14:56:30 +0530

    finished

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,24 +1,27 @@
-import { useEffect } from "react";
-import { useState } from "react";
+
+import { useEffect, useState} from "react";
 
 export default function Advice() {
+
     const [Advice, setAdvice] = useState("Please Click the Button for Advice");
-    const [count, setCount] = useState(0);
-    async function getAdvice() {
+    const [count, setCount] = useState(-1);
+
+    async function getAdvices() {
         const res = await fetch("https://api.adviceslip.com/advice");
         const data = await res.json();
         setAdvice(data.slip.advice);
-        setCount((c)=> c + 1)    
-    }
+        setCount((c) => c + 1);
+    };
+
     useEffect(() => {
-      getAdvice();
-    }, [ ])
-    
-  return (
-    <div className=" min-vw-100 min-vh-100 text-bg-success d-flex justify-content-center align-items-center">
-        <h3>{Advice}</h3>
-        <button className=" btn btn-lg btn-outline-primary" type="button" onClick={getAdvice}>Click Me</button>
-        <h2>You Get {count} advice</h2>
-    </div>
-  )
-}
+        getAdvices();
+    }, []);
+
+    return (
+        <div className="min-vw-100 min-vh-100 text-bg-success d-flex flex-column justify-content-center align-items-center">
+            <h2>{Advice}</h2>
+            <button className=" btn btn-lg btn-primary m-4" type="button" onClick={getAdvices}>Click Me</button>
+            <h3>You have read <span className=" text-bg-info rounded-3">{count}</span> pieces of advice</h3>
+        </div>
+    )
+};
