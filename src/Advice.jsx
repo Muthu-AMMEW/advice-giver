665db0139cfda1a6460deb2e20b30b9471734d5a
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-11-12 12:37:34 +0530

    layout change

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import './Advice.css';
 
 
@@ -20,10 +20,18 @@ export default function Advice() {
     }, []);
 
     return (
-        <div className="min-vw-100 min-vh-100 bgPic text-white d-flex flex-column justify-content-center align-items-center p-4">
-            <h2>{Advice}</h2>
-            <button className=" btn btn-lg btn-primary m-4" type="button" onClick={getAdvice}>Click Me</button>
-            <h3>You have read <span className=" text-bg-info rounded-3">{count}</span> pieces of advice</h3>
+        <div className="row min-vw-100 min-vh-100 justify-content-center align-items-center bg-info-subtle bg-image">
+            <div className="col-11 col-sm-8 col-md-7 col-lg-6 col-xl-5">
+
+                <div className="bg-white d-flex flex-column justify-content-center align-items-center w-100 rounded-5">
+                    <div className='text-center text-success h2 mt-2'>Advice Giver</div>
+
+                    <h5 className="text-center p-3">{Advice}</h5>
+                    <button className=" btn btn-lg btn-primary" type="button" onClick={getAdvice}>Click Me</button>
+                    <h6 className="text-center p-3">You have read <span className=" text-bg-info rounded-3">{count}</span> pieces of advice</h6>
+                    <a href="https://www.linkedin.com/in/muthu-ammew" className="text-black text-decoration-none mb-2">Designed by <span className="text-success text-decoration-underline fw-bolder">Muthu</span></a>
+                </div>
+            </div>
         </div>
     )
 };
